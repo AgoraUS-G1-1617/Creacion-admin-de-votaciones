9b2c4dd8b2e5d95295141949954fbca4f1d79449
Ismael de la Ossa Puerto <ismaelcarmona1995@hotmail.com>
2016-12-14 11:49:21 +0100

    Reimportación entidades ApiController

--- src/main/java/controllers/APIController.java
+++ src/main/java/controllers/APIController.java
@@ -1,24 +1,15 @@
 package controllers;
 
 import java.util.Collection;
-import java.util.Date;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.Assert;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import services.QuestionService;
 import services.SurveyService;
-import domain.CheckToken;
-import domain.Question;
 import domain.Survey;
 
 /**
@@ -36,11 +27,6 @@ public class APIController {
 
 	@Autowired
 	private SurveyService surveyService;
-
-	@Autowired
-	private QuestionService questionService;
-
-
 
 	/**
 	 * @return Constructor del Controlador.
