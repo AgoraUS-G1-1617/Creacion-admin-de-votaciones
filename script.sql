0e8a9c9a52bcc96b71f7c64bf3aa2540392d1683
Rafael Trujillo <raftrugon@users.noreply.github.com>
2017-01-04 21:18:16 +0100

    actualización del script.sql
    se ha actualizado el script.sql para tratar de solucionar el fallo a la hora de acceder a la base de datos de forma remota

--- script.sql
+++ script.sql
@@ -1,11 +1,9 @@
 drop database if exists `CreacionAdminVotaciones`;
 create database `CreacionAdminVotaciones`;
 
-
-
 create user 'acme-user'@'%' identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
-create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
 
+create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
 
 grant select, insert, update, delete 
 	on `CreacionAdminVotaciones`.* to 'acme-user'@'%';
@@ -35,68 +33,68 @@ USE `CreacionAdminVotaciones`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `domainentity`
+-- Table structure for table `domain_entity`
 --
 
-DROP TABLE IF EXISTS `domainentity`;
+DROP TABLE IF EXISTS `domain_entity`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `domainentity` (
-  `DTYPE` varchar(31) NOT NULL,
+CREATE TABLE `domain_entity` (
+  `dtype` varchar(31) NOT NULL,
   `id` int(11) NOT NULL,
   `version` int(11) NOT NULL,
-  `surveyId` int(11) DEFAULT NULL,
+  `survey_id` int(11) DEFAULT NULL,
   `text` varchar(255) DEFAULT NULL,
-  `census` int(11) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `endDate` datetime DEFAULT NULL,
-  `postalCode` varchar(255) DEFAULT NULL,
-  `startDate` datetime DEFAULT NULL,
-  `tipo` varchar(255) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `usernameCreator` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
+  `census` int(11) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
+  `postal_code` varchar(255) DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `tipo` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `username_creator` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_7c4bo9w7vkdy6tnl6i4lsxcwc` (`username`)
+  UNIQUE KEY `UK_mjkjds1f2jnib0fgvfkg5a44h` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `domainentity`
+-- Dumping data for table `domain_entity`
 --
 
-LOCK TABLES `domainentity` WRITE;
-/*!40000 ALTER TABLE `domainentity` DISABLE KEYS */;
-INSERT INTO `domainentity` VALUES ('Survey',1,0,NULL,NULL,1,'Indaga sobre las preferencias de los ciudadanos en los equipos de la ciudad','2016-06-02 12:00:00','41005','2016-05-02 12:00:00','Cerrado','Encuesta futbolística en Sevilla','Verde1',NULL,NULL),('Question',2,0,1,'¿Cual es su equipo de fútbol favorito?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('Question',3,0,1,'¿Prefiere a Sevilla o Betis?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('Question',4,0,1,'¿Tiene problemas mentales?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('Survey',9,0,NULL,NULL,1,'Pretendemos conocer si la gente cree que ya hemos superado la crisis','2017-04-11 12:00:00','11393','2017-03-01 12:00:00','Cerrado','Crisis','Verde1',NULL,NULL),('Question',10,0,3,'No',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('Question',11,0,3,'Si',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-/*!40000 ALTER TABLE `domainentity` ENABLE KEYS */;
+LOCK TABLES `domain_entity` WRITE;
+/*!40000 ALTER TABLE `domain_entity` DISABLE KEYS */;
+INSERT INTO `domain_entity` VALUES ('Survey',1,0,NULL,NULL,NULL,NULL,1,'Indaga sobre las preferencias de los ciudadanos en los equipos de la ciudad','2016-06-02 12:00:00','41005','2016-05-02 12:00:00','Cerrado','Encuesta futbolística en Sevilla','Verde1'),('Question',2,0,1,'¿Cual es su equipo de fútbol favorito?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('Question',3,0,1,'¿Prefiere a Sevilla o Betis?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('Question',4,0,1,'¿Tiene problemas mentales?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `domain_entity` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `domainentity_domainentity`
+-- Table structure for table `domain_entity_questions`
 --
 
-DROP TABLE IF EXISTS `domainentity_domainentity`;
+DROP TABLE IF EXISTS `domain_entity_questions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `domainentity_domainentity` (
-  `DomainEntity_id` int(11) NOT NULL,
-  `questions_id` int(11) NOT NULL,
-  UNIQUE KEY `UK_d0uimbuu5c0k0did565pmg90c` (`questions_id`),
-  KEY `FK_lwtddf7ju3ihywlna8nvxu2q6` (`DomainEntity_id`),
-  CONSTRAINT `FK_lwtddf7ju3ihywlna8nvxu2q6` FOREIGN KEY (`DomainEntity_id`) REFERENCES `domainentity` (`id`),
-  CONSTRAINT `FK_d0uimbuu5c0k0did565pmg90c` FOREIGN KEY (`questions_id`) REFERENCES `domainentity` (`id`)
+CREATE TABLE `domain_entity_questions` (
+  `domain_entity` int(11) NOT NULL,
+  `questions` int(11) NOT NULL,
+  UNIQUE KEY `UK_7wb3r06mfugp9b6wpfmuiw8bj` (`questions`),
+  KEY `FK_3b5miul0r1933n3qt774j1dje` (`domain_entity`),
+  CONSTRAINT `FK_3b5miul0r1933n3qt774j1dje` FOREIGN KEY (`domain_entity`) REFERENCES `domain_entity` (`id`),
+  CONSTRAINT `FK_7wb3r06mfugp9b6wpfmuiw8bj` FOREIGN KEY (`questions`) REFERENCES `domain_entity` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `domainentity_domainentity`
+-- Dumping data for table `domain_entity_questions`
 --
 
-LOCK TABLES `domainentity_domainentity` WRITE;
-/*!40000 ALTER TABLE `domainentity_domainentity` DISABLE KEYS */;
-INSERT INTO `domainentity_domainentity` VALUES (1,2),(1,3),(1,4),(9,10),(9,11);
-/*!40000 ALTER TABLE `domainentity_domainentity` ENABLE KEYS */;
+LOCK TABLES `domain_entity_questions` WRITE;
+/*!40000 ALTER TABLE `domain_entity_questions` DISABLE KEYS */;
+INSERT INTO `domain_entity_questions` VALUES (1,2),(1,3),(1,4);
+/*!40000 ALTER TABLE `domain_entity_questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -118,32 +116,32 @@ CREATE TABLE `hibernate_sequences` (
 
 LOCK TABLES `hibernate_sequences` WRITE;
 /*!40000 ALTER TABLE `hibernate_sequences` DISABLE KEYS */;
-INSERT INTO `hibernate_sequences` VALUES ('DomainEntity',1);
+INSERT INTO `hibernate_sequences` VALUES ('domain_entity',2);
 /*!40000 ALTER TABLE `hibernate_sequences` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `useraccount_authorities`
+-- Table structure for table `user_account_authorities`
 --
 
-DROP TABLE IF EXISTS `useraccount_authorities`;
+DROP TABLE IF EXISTS `user_account_authorities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `useraccount_authorities` (
-  `UserAccount_id` int(11) NOT NULL,
+CREATE TABLE `user_account_authorities` (
+  `user_account` int(11) NOT NULL,
   `authority` varchar(255) DEFAULT NULL,
-  KEY `FK_b63ua47r0u1m7ccc9lte2ui4r` (`UserAccount_id`),
-  CONSTRAINT `FK_b63ua47r0u1m7ccc9lte2ui4r` FOREIGN KEY (`UserAccount_id`) REFERENCES `domainentity` (`id`)
+  KEY `FK_pao8cwh93fpccb0bx6ilq6gsl` (`user_account`),
+  CONSTRAINT `FK_pao8cwh93fpccb0bx6ilq6gsl` FOREIGN KEY (`user_account`) REFERENCES `domain_entity` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `useraccount_authorities`
+-- Dumping data for table `user_account_authorities`
 --
 
-LOCK TABLES `useraccount_authorities` WRITE;
-/*!40000 ALTER TABLE `useraccount_authorities` DISABLE KEYS */;
-/*!40000 ALTER TABLE `useraccount_authorities` ENABLE KEYS */;
+LOCK TABLES `user_account_authorities` WRITE;
+/*!40000 ALTER TABLE `user_account_authorities` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_account_authorities` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -155,4 +153,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-12-27 10:41:24
+-- Dump completed on 2017-01-04 20:58:36
