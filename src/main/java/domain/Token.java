a468ae52d1cca0137d0173170b078eeb01c2ded8
ismaelcarmona1995@hotmail.com <ismaelcarmona1995@hotmail.com>
2017-01-08 19:13:12 +0100

    Añadido al domain el Token.java

---
+++ src/main/java/domain/Token.java
@@ -0,0 +1,28 @@
+package domain;
+
+import javax.validation.constraints.NotNull;
+
+public class Token {
+
+	// Constructors ------------------------------------------------------------
+	
+	public Token() {
+		super();
+	}
+
+	// Attributes -------------------------------------------------------------
+	
+	boolean valid;
+
+	@NotNull
+	public boolean isValid() {
+		return valid;
+	}
+
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+
+	// Relationships ----------------------------------------------------------
+
+}
